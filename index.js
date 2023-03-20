9c00f0391784fffb27245c76ce65d9922fe18df9
Md Baker Hossain <badhonthasana6n4ia@gmail.com>
2023-03-20 20:44:49 +0600

    complit slder

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 const images = [
-    download (1).jpg,
-    download (2).jpg,
-    download (3).jpg,
-    download (4).jpg,
-    download (5).jpg,
-    download (6).jpg
+   " download (1).jpg",
+   "download (2).jpg",
+    "download (3).jpg",
+    "download (4).jpg",
+    "download (5).jpg",
+    "download.jpg",
+     
 ]
 
-
+const imagesArea = document.getElementById("slider");
+let imageIndex = 0
 setInterval(()=>{
-        
+    if(imageIndex === images.length){
+        imageIndex = 0
+    }
+        const imageUrl = images[imageIndex];
+        imageIndex++;
+        imagesArea.src=imageUrl;
+        console.log(imageUrl)
 },1000)
