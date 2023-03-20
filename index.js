635358763d7d49cfe3bd8c2e43962b10e35a575a
Md Baker Hossain <badhonthasana6n4ia@gmail.com>
2023-03-20 20:28:50 +0600

    fist commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const images = [
+    download (1).jpg,
+    download (2).jpg,
+    download (3).jpg,
+    download (4).jpg,
+    download (5).jpg,
+    download (6).jpg
+]
+
+
+setInterval(()=>{
+        
+},1000)
